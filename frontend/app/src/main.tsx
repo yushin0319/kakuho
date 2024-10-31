93e08c4db6c8143837fa2f16fecf507dcce4877d
yushin0319 <yushin.fudo1986@gmail.com>
2024-10-31 14:31:31 +0900

    change scss "import" to "use"

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./assets/styles/_global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
