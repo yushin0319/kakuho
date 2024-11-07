f9bfe74c082a09e96216caff467896f8f64d9a21
yushin0319 <yushin.fudo1986@gmail.com>
2024-11-07 15:02:35 +0900

    adjust managelist

--- frontend/app/src/pages/ManageList.tsx
+++ frontend/app/src/pages/ManageList.tsx
@@ -1,4 +1,3 @@
-// app/src/components/ManageList.tsx
 import { useEffect, useState } from "react";
 import { fetchEvents } from "../services/api/event";
 import { fetchEventStages } from "../services/api/stage";
@@ -24,7 +23,6 @@ const ManageList = () => {
 
   const { reservations, isLoading } = useReservationContext();
 
-  // イベント、ステージ、シートグループのデータをロードする
   const loadEventsData = async () => {
     try {
       const eventsData = await fetchEvents();
@@ -33,7 +31,6 @@ const ManageList = () => {
       const stageData: { [key: number]: StageResponse[] } = {};
       const seatGroupData: { [key: number]: SeatGroupResponse[] } = {};
 
-      // 各イベントごとにステージとシートグループを取得
       for (const event of eventsData) {
         const stages = await fetchEventStages(event.id);
         stageData[event.id] = stages;
@@ -55,21 +52,27 @@ const ManageList = () => {
     loadEventsData();
   }, []);
 
-  // 開閉制御の関数
   const toggleEventOpen = (eventId: number) => {
-    setOpenEventIds((prev) =>
-      prev.includes(eventId)
-        ? prev.filter((id) => id !== eventId)
-        : [...prev, eventId]
-    );
+    setOpenEventIds((prev) => {
+      const isOpen = prev.includes(eventId);
+      if (isOpen) {
+        // 親イベントが閉じるときに、関連するステージもすべて閉じる
+        setOpenStageIds([]);
+        setOpenSeatGroupIds([]);
+      }
+      return isOpen ? prev.filter((id) => id !== eventId) : [...prev, eventId];
+    });
   };
 
   const toggleStageOpen = (stageId: number) => {
-    setOpenStageIds((prev) =>
-      prev.includes(stageId)
-        ? prev.filter((id) => id !== stageId)
-        : [...prev, stageId]
-    );
+    setOpenStageIds((prev) => {
+      const isOpen = prev.includes(stageId);
+      if (isOpen) {
+        // ステージが閉じるときに、関連するシートグループもすべて閉じる
+        setOpenSeatGroupIds([]);
+      }
+      return isOpen ? prev.filter((id) => id !== stageId) : [...prev, stageId];
+    });
   };
 
   const toggleSeatGroupOpen = (seatGroupId: number) => {
@@ -95,8 +98,12 @@ const ManageList = () => {
           >
             {openEventIds.includes(event.id) ? "−" : "+"} {event.name}
           </div>
-          {openEventIds.includes(event.id) &&
-            stages[event.id]?.map((stage) => (
+          <div
+            className={`stages ${
+              openEventIds.includes(event.id) ? "open" : ""
+            }`}
+          >
+            {stages[event.id]?.map((stage) => (
               <div
                 key={stage.id}
                 className={`stage ${
@@ -111,8 +118,12 @@ const ManageList = () => {
                   {getDate(new Date(stage.start_time))}{" "}
                   {getHour(new Date(stage.start_time))}
                 </div>
-                {openStageIds.includes(stage.id) &&
-                  seatGroups[stage.id]?.map((seatGroup) => {
+                <div
+                  className={`seat-groups ${
+                    openStageIds.includes(stage.id) ? "open" : ""
+                  }`}
+                >
+                  {seatGroups[stage.id]?.map((seatGroup) => {
                     const ticketTypeNames = reservations
                       .filter((res) => res.seatGroup.id === seatGroup.id)
                       .map((res) => res.ticketType.type_name)
@@ -134,37 +145,37 @@ const ManageList = () => {
                           {openSeatGroupIds.includes(seatGroup.id) ? "−" : "+"}{" "}
                           {ticketTypeNames} 残{seatGroup.capacity}席
                         </div>
-                        {openSeatGroupIds.includes(seatGroup.id) && (
-                          <div
-                            className={`reservations ${
-                              openSeatGroupIds.includes(seatGroup.id)
-                                ? "open"
-                                : ""
-                            }`}
-                          >
-                            {reservations
-                              .filter(
-                                (data) => data.seatGroup.id === seatGroup.id
-                              )
-                              .map((data) => (
-                                <div
-                                  key={data.reservation.id}
-                                  className="reservation"
-                                >
-                                  {data.ticketType.type_name}{" "}
-                                  {data.reservation.num_attendees}枚{" "}
-                                  {data.reservation.num_attendees *
-                                    data.ticketType.price}
-                                  円 {data.user.nickname || data.user.email}
-                                </div>
-                              ))}
-                          </div>
-                        )}
+                        <div
+                          className={`reservations ${
+                            openSeatGroupIds.includes(seatGroup.id)
+                              ? "open"
+                              : ""
+                          }`}
+                        >
+                          {reservations
+                            .filter(
+                              (data) => data.seatGroup.id === seatGroup.id
+                            )
+                            .map((data) => (
+                              <div
+                                key={data.reservation.id}
+                                className="reservation"
+                              >
+                                {data.ticketType.type_name}{" "}
+                                {data.reservation.num_attendees}枚{" "}
+                                {data.reservation.num_attendees *
+                                  data.ticketType.price}
+                                円 {data.user.nickname || data.user.email}
+                              </div>
+                            ))}
+                        </div>
                       </div>
                     );
                   })}
+                </div>
               </div>
             ))}
+          </div>
         </div>
       ))}
     </div>
