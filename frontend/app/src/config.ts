5abd3ada15678f53fb5b77bce581afc21529bca1
yushin0319 <yushin.fudo1986@gmail.com>
2025-01-18 11:02:38 +0900

    rewrite python version

---
+++ frontend/app/src/config.ts
@@ -0,0 +1,6 @@
+//src/config.ts
+const config = {
+  BASE_API_URL: import.meta.env.VITE_APP_API_URL || "http://127.0.0.1:8000/",
+};
+
+export default config;
--- frontend/app/src/services/api/api.ts
+++ frontend/app/src/services/api/api.ts
@@ -1,8 +1,10 @@
+// app/src/services/api/api.ts
 import axios from "axios";
+import config from "../../config";
 
 // APIベースURL設定
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/", // バックエンドのベースURL
+  baseURL: config.BASE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
